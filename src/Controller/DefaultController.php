c63cf8e4c5584a0e872bd0bb708bbc4b5495d546
Jess Lutheen <jnicolleau@gmail.com>
2021-12-10 14:16:53 +0100

    initial commit

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function showAction()
+    {
+        return $this->render('index.html.twig', ['toto' => 'tata']);
+
+    }
+}
